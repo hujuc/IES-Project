28c3cdfdfad5da999c0fb4cc9c55ddef5c6dbe5d
angelammaribeiro <angelammaribeiro@ua.pt>
2024-10-21 22:33:44 +0100

    NavBar done

--- projHomeMaid/frontend/src/components/NavBar.jsx
+++ projHomeMaid/frontend/src/components/NavBar.jsx
@@ -1,59 +1,20 @@
-
-function NavBar(){
-    return(
-        <div className="navbar bg-base-100">
-            <div className="navbar-start">
-                <div className="dropdown">
-                <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                    <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M4 6h16M4 12h8m-8 6h16" />
-                    </svg>
-                </div>
-                <ul
-                    tabIndex={0}
-                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                    <li><a>Item 1</a></li>
-                    <li>
-                    <a>Parent</a>
-                    <ul className="p-2">
-                        <li><a>Submenu 1</a></li>
-                        <li><a>Submenu 2</a></li>
-                    </ul>
-                    </li>
-                    <li><a>Item 3</a></li>
-                </ul>
-                </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                <li><a>Item 1</a></li>
-                <li>
-                    <details>
-                    <summary>Parent</summary>
-                    <ul className="p-2">
-                        <li><a>Submenu 1</a></li>
-                        <li><a>Submenu 2</a></li>
-                    </ul>
-                    </details>
-                </li>
-                <li><a>Item 3</a></li>
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
-            </div>
+function NavBar() {
+    return (
+      <div className="navbar bg-base-100 shadow-lg px-4">
+        {/* Navbar Start (Logo) */}
+        <div className="navbar-start">
+          {/* Logo */}
+          <img src="/public/logo.png" className="w-20 h-20 lg:w-32 lg:h-32 mx-2" alt="HomeMaid Logo" />
         </div>
+  
+        {/* Navbar End (Buttons) */}
+        <div className="navbar-end space-x-4">
+          <a className="btn bg-orange-500 text-white hover:bg-orange-600 transition-all">Sign Up</a>
+          <a className="btn border border-orange-500 text-orange-500 hover:bg-orange-100 transition-all">Log In</a>
+        </div>
+      </div>
     );
-}
-
-export default NavBar
+  }
+  
+  export default NavBar;
+  

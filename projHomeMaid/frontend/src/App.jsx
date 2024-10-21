050202fa86fbfb69223d0cff4a19dde0dce53031
angelammaribeiro <angelammaribeiro@ua.pt>
2024-10-21 22:45:47 +0100

    Stats

--- projHomeMaid/frontend/src/App.jsx
+++ projHomeMaid/frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import NavBar from "./components/NavBar"
 import PhotoCard from "./components/PhotoCard"
+import Stats from "./components/Stats";
 
 function App() {
   return (
@@ -13,6 +14,9 @@ function App() {
         <div className="w-full md:w-1/2 flex-grow">
           <PhotoCard />
         </div>
+      </div>
+      <div>
+        <Stats></Stats>
       </div>
     </>
   )

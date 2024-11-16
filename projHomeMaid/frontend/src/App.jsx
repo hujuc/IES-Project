5e224b5103ff727d79fce93bc5f01e6cf1140a98
Rita-Silva04 <aritafs@ua.pt>
2024-11-16 21:34:39 +0000

    login frontend done

--- projHomeMaid/frontend/src/App.jsx
+++ projHomeMaid/frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
-import NavBar from "./components/NavBar";
-import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
 
 function App() {
   return (
     <>
-      <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} /> {/* Definir a rota para a homepage */}
+        <Route path="/" element={<Login />} /> {/* Definir a rota para a homepage */}
       </Routes>
     </>
   )

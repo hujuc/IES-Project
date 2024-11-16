079ed2c31da15e758d9ac58af68558f93ceb4604
Rita-Silva04 <aritafs@ua.pt>
2024-11-16 17:46:19 +0000

    Welcome Page completed and organization of folders done

---
+++ projHomeMaid/frontend/src/components/global/Layout.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import NavBar from './NavBar';
+import Footer from './Footer';
+
+function Layout({ children }) {
+    return (
+        <div className="bg-white min-h-screen flex flex-col">
+            {/* Navbar at the top */}
+            <NavBar />
+
+            {/* Main content area with flex-grow */}
+            <div className="flex-grow flex flex-col items-center">
+                {children}
+            </div>
+
+            {/* Footer at the bottom */}
+            <Footer />
+        </div>
+    );
+}
+
+export default Layout;

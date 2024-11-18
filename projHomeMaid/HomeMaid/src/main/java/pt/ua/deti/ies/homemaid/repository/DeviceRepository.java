ee71cb317c91797de3514c53591a869e3f979d9d
Rita-Silva04 <aritafs@ua.pt>
2024-11-18 10:43:20 +0000

    final backend restructoring

--- projHomeMaid/HomeMaid/src/main/java/pt/ua/deti/ies/homemaid/repository/DeviceRepository.java
+++
@@ -1,7 +0,0 @@
-package pt.ua.deti.ies.homemaid.repository;
-
-import pt.ua.deti.ies.homemaid.model.Device;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DeviceRepository extends MongoRepository<Device, String> {
-}
--- projHomeMaid/HomeMaid/src/main/java/pt/ua/deti/ies/homemaid/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package pt.ua.deti.ies.homemaid.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import pt.ua.deti.ies.homemaid.model.Room;
-
-public interface RoomRepository extends MongoRepository<Room, String> {
-}

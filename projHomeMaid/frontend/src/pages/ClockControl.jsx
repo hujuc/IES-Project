ebbc525c2846b7f3ac94414422bb1cfa22155c70
angelammaribeiro <angelammaribeiro@ua.pt>
2024-11-18 13:08:47 +0000

    Applied stashed changes to feature/fullStack-automationpages-57

---
+++ projHomeMaid/frontend/src/pages/ClockControl.jsx
@@ -0,0 +1,10 @@
+export default function ClockControl(){
+    return(
+        <div
+            className="relative flex flex-col items-center w-screen min-h-screen"
+            style={{ backgroundColor: "#433F3C" }}
+        >
+
+        </div>
+    )
+}
--- projHomeMaid/frontend/tailwind.config.js
+++ projHomeMaid/frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'home-maid': "url('public/HomeMaid.png')", // Path to your image
+      },
+    },
   },
   plugins: [
     require('daisyui'),

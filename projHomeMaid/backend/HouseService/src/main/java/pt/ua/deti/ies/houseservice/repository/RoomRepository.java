68b709c6bd6ba5ca588070d23424347b6daf58b9
Rita-Silva04 <aritafs@ua.pt>
2024-11-19 14:03:20 +0000

    merge correct backend to this branch

--- projHomeMaid/backend/HouseService/src/main/java/pt/ua/deti/ies/houseservice/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package pt.ua.deti.ies.houseservice.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import pt.ua.deti.ies.houseservice.model.Room;
-
-public interface RoomRepository extends MongoRepository<Room, String> {
-}
---
+++ projHomeMaid/backend/src/main/java/pt/ua/deti/ies/backend/controller/NotificationController.java
@@ -0,0 +1,43 @@
+package pt.ua.deti.ies.homemaid.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pt.ua.deti.ies.homemaid.model.Notification;
+import pt.ua.deti.ies.homemaid.service.NotificationService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users/{username}/notifications")
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Notification> createNotification(@PathVariable("username") String username, @RequestBody Notification notification) {
+        Notification createdNotification = notificationService.createNotification(username, notification);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdNotification);
+    }
+
+    @GetMapping
+    public List<Notification> getUserNotifications(@PathVariable("username") String username) {
+        return notificationService.getNotificationsByUserName(username);
+    }
+
+    @PutMapping("/{notificationId}/read")
+    public ResponseEntity<Void> markNotificationAsRead(@PathVariable("username") String username, @PathVariable String notificationId) {
+        notificationService.markAsRead(notificationId, username);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{notificationId}")
+    public ResponseEntity<Void> deleteNotification(@PathVariable("username") String username, @PathVariable String notificationId) {
+        notificationService.deleteNotification(notificationId, username);
+        return ResponseEntity.noContent().build();
+    }
+}

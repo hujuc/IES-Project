71b4cea5a4cd16825a386c11333924acfcbe4817
angelammaribeiro <angelammaribeiro@ua.pt>
2024-11-19 18:48:20 +0000

    Resolvendo conflito no arquivo workspace.xml

--- projHomeMaid/frontend/src/pages/SignUp.jsx
+++ projHomeMaid/frontend/src/pages/SignUp.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../components/Global/Layout';
+import Layout from '../components/global/Layout';
 import SignUpForm from '../components/SignUpPage/SignUpForm';
 
 function SignUp() {
--- projHomeMaid/frontend/vite.config.js
+++ projHomeMaid/frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      axios: 'axios', // No need to use require.resolve
+    },
+  },
   plugins: [react()],
-})
+});

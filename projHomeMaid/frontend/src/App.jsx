843433234c331e5542fc5ed5698adc08033c53ab
angelammaribeiro <angelammaribeiro@ua.pt>
2024-11-19 18:48:20 +0000

    Resolvendo conflito no arquivo workspace.xml

--- projHomeMaid/frontend/src/App.jsx
+++ projHomeMaid/frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Welcome from "./pages/Welcome.jsx";
 import Login from "./pages/Login.jsx";
+import SignUp from "./pages/SignUp.jsx";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<SignUp />} />
       </Routes>
     </>
   )

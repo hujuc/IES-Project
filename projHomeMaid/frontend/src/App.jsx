376f15947f03214f0feb79fdea31447770c7af9d
Rita-Silva04 <aritafs@ua.pt>
2024-11-20 16:07:57 +0000

    welcome page (navbar and footer) endpoints perfect

--- projHomeMaid/frontend/src/App.jsx
+++ projHomeMaid/frontend/src/App.jsx
@@ -2,11 +2,24 @@ import { Routes, Route } from 'react-router-dom';
 import Login from "./pages/Login.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import HomePage from "./pages/HomePage.jsx";
+import Welcome from "./pages/Welcome.jsx";
+import AboutUs from "./pages/AboutUs.jsx";
+import Help from "./pages/Help.jsx";
 
 function App() {
   return (
     <>
       <Routes>
+          {/* Rota inicial */}
+          <Route path="/" element={<Welcome />} />
+
+          {/* Rota para o AboutUs  */}
+          <Route path="/aboutUs" element={<AboutUs />} />
+
+          {/* Rota para o Help  */}
+          <Route path="/help" element={<Help />} />
+
+          {/* Rota para signUp */}
           <Route path="/signUp" element={<SignUp />} />
 
           {/* Rota para Login */}
--- projHomeMaid/frontend/src/components/global/NavBar.jsx
+++ projHomeMaid/frontend/src/components/global/NavBar.jsx
@@ -1,13 +1,37 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
 function NavBar() {
+    const navigate = useNavigate(); // Hook para redirecionar
+
     return (
         <div className="navbar bg-white shadow-lg px-2 py-2 sticky top-0 z-50">
             <div className="navbar-start">
-                <img src="/public/logo.png" className="w-12 h-12 lg:w-16 lg:h-16 mx-2" alt="HomeMaid Logo" />
+                {/* Clique no logo redireciona para "/" */}
+                <img
+                    src="/public/logo.png"
+                    className="w-12 h-12 lg:w-16 lg:h-16 mx-2 cursor-pointer"
+                    alt="HomeMaid Logo"
+                    onClick={() => navigate("/")} // Redireciona para "/"
+                />
             </div>
 
             <div className="navbar-end space-x-2">
-                <a className="btn bg-orange-500 text-white hover:bg-orange-600 transition-all border-none px-3 py-1">Sign Up</a>
-                <a className="btn bg-white border border-orange-500 text-orange-500 hover:bg-orange-100 transition-all px-3 py-1">Log In</a>
+                {/* Botão Sign Up */}
+                <button
+                    className="btn bg-orange-500 text-white hover:bg-orange-600 transition-all border-none px-3 py-1"
+                    onClick={() => navigate("/signUp")}
+                >
+                    Sign Up
+                </button>
+
+                {/* Botão Log In */}
+                <button
+                    className="btn bg-white border border-orange-500 text-orange-500 hover:bg-orange-100 transition-all px-3 py-1"
+                    onClick={() => navigate("/login")}
+                >
+                    Log In
+                </button>
             </div>
         </div>
     );

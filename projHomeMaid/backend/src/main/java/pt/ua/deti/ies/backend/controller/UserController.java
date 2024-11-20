bdb70b0917b207f8dabb26194c4de5b4fee22e22
Rita-Silva04 <aritafs@ua.pt>
2024-11-20 01:51:06 +0000

    devlopment env sem CORS problems

--- projHomeMaid/backend/src/main/java/pt/ua/deti/ies/backend/controller/UserController.java
+++ projHomeMaid/backend/src/main/java/pt/ua/deti/ies/backend/controller/UserController.java
@@ -8,6 +8,7 @@ import pt.ua.deti.ies.homemaid.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 

f9e49138b8c099a39b5a62ecb9799fed539d60e6
Rita-Silva04 <aritafs@ua.pt>
2024-11-20 15:30:13 +0000

    just change the route for the signUp page

--- projHomeMaid/frontend/src/App.jsx
+++ projHomeMaid/frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Routes>
-          <Route path="/" element={<SignUp />} />
+          <Route path="/signUp" element={<SignUp />} />
 
           {/* Rota para Login */}
           <Route path="/login" element={<Login />} />
